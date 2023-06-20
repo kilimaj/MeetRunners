39cdfc48485cd2080faea5f100ae8ace448434d2
John Kilima <jkbundala@gmail.com>
2023-06-20 12:04:43 +0300

    Initial Project setup and database connection

---
+++ src/main/java/com/example/MeetRunners/MeetRunnersApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeetRunners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetRunnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetRunnersApplication.class, args);
+	}
+
+}
